4e6b6845f45017aeb46c875ebb5854c6ed7b5546
LoaiAkram <lolyakram11@gmail.com>
2019-12-19 22:10:49 -0800

    added layout

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
